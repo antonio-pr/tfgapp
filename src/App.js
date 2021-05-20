1fad4129cc5857c6196eb01e88eee60889ae5941
antonio-pr <56087017+antonio-pr@users.noreply.github.com>
2021-05-20 23:48:00 +0200

    Several sensors representation implemented

--- src/App.js
+++ src/App.js
@@ -2,51 +2,199 @@ import React, { useState, useEffect, useRef } from 'react';
 import LineChart from './components/LineChart'
 import './App.css';
 import mqtt from 'mqtt';
+import 'fontsource-roboto'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from "@material-ui/core/styles";
+import {} from '@material-ui/core/colors/'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: theme.palette.success.main
+  }
+}));
 
 function App() {
-  const [state, setState] = useState({});
-  const client = useRef(null);
 
-  useEffect(() => {
-    client.current = mqtt.connect('mqtt://broker.hivemq.com:8000/mqtt')
-  },[]);
+    const [data, setData] = useState({
+        lvl: null,
+        sensor: "",
+        date: []
+    });
 
-  useEffect(() => {
-    const handleJsonMessage = (json,sensor_name) => {
-      setState({
-        lvl: json["co2"],
-        sensor: sensor_name
-      });
-      console.log(state)
+    const [chartData, setChartData] = useState({
+        pos: null,
+        labels: [[]],
+        datasets: [
+            {
+                label: "",
+                data: [],
+                backgroundColor: [
+                'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 2,
+            }
+        ]
+    })
+    const client = useRef(null);
+
+    const datasets_structure = (label, data) => {
+        return({
+            label: label,
+            data: data,
+            backgroundColor: [
+            'rgba(75, 192, 192, 0.2)'
+            ],
+            borderColor: [
+            'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 2,
+        })
     }
 
-    client.current.on("connect", () => {
-      console.log("Connected");
-      client.current.subscribe("/tfg/react/chart/co2/#");
-    });
+    const update_dataset = (data,newValue) => {
+        var data_aux = data.slice();
+        if(data.length < 10) {
+            data_aux.push(newValue);
+        }
+        else {
+            for (var i = 0; i < 9; i++) {
+                data_aux[i] = data_aux[i+1];
+            }
+            data_aux[9] = newValue;
+        }
+        return data_aux
+    }
 
-    client.current.on("message", (topic, message) => {
-        let regex = /(sensor\d+)/g;
-        let coincidence = topic.match(regex);
+    const build_data = (lvl, sensor, date) => {
+        let data_aux = {
+            ...chartData
+        }
+        // console.log("data_aux:", data_aux)
 
-        // handleJsonMessage(JSON.parse(message.toString()),coincidence[0]);
+        if(chartData.datasets[0].label === "") {
+            data_aux.pos = 0
+            data_aux.datasets[0].label = sensor;
+            data_aux.datasets[0].data = [lvl];
+            data_aux.labels = [[date]];
+            console.log("data_aux: ", data_aux)
+        }
+        else{
+            var pos = null;
+            for(var i=0; i<chartData.datasets.length; i++) {
+                //console.log("bucle: ", chartData.datasets[i].label)
+                if(chartData.datasets[i].label === sensor) {
+                    pos = i;
+                }
+            }
 
-        setState({
-          lvl: JSON.parse(message.toString())["co2"],
-          sensor: coincidence[0]
+            //pos = chartData.datasets.indexOf(sensor);
+
+            if (pos == null){
+                data_aux.pos = data_aux.labels.length
+                data_aux.datasets.push(datasets_structure(sensor,[lvl]));
+                data_aux.labels.push([date])
+            }
+            else{
+                let data_returned = update_dataset(data_aux.datasets[pos].data,lvl)
+                data_aux.pos = pos
+                data_aux.datasets[pos] = datasets_structure(sensor,data_returned)
+                data_aux.labels[pos] = update_dataset(data_aux.labels[pos],date);
+                console.log("print data_aux:", data_aux)
+            }
+        }
+
+        return data_aux
+    }
+
+    const procedure = (client) => {
+        client.on("connect", () => {
+        console.log("Connected");
+        client.subscribe("/tfg/react/chart/co2/#");
         });
-        console.log(state)
-    });
-  },[state]);
 
-  //<LineChart state={state} /> 
-  return (
-    <div className="App">
-      <header className="App-header">
+        client.on("message", (topic, message) => {
+            console.log("on message");
+            let regex = /(sensor\d+)/g;
+            let sensor_aux = topic.match(regex)[0];
+            let lvl_aux = JSON.parse(message.toString())["co2"];
+            var hoy = new Date();
+            var h = hoy.getHours().toString();
+            var m = hoy.getMinutes().toString();
+            var s = hoy.getSeconds().toString();
+            var date_aux = h.concat(":",m,":",s);
+            // console.log("hoy:", hoy);
 
-      </header>
-    </div>
-  );
+            // let data_aux = {
+            //     ...chartData
+            // }
+    
+            // console.log("data_aux:", data_aux)
+            
+            setChartData(build_data(lvl_aux,sensor_aux, date_aux))
+
+            setData((prev) => ({
+                ...prev, 
+                lvl: lvl_aux,
+                sensor: sensor_aux,
+                date: date_aux
+            }));
+
+            
+        });
+    }
+
+    useEffect(() => {
+        client.current = mqtt.connect('mqtt://broker.hivemq.com:8000/mqtt')
+        procedure(client.current) 
+
+        return () => client.current.end();
+    },[procedure]);
+
+    useEffect(() => {
+        console.log("useEffect_Data", chartData);
+
+    },[chartData]);
+
+/* <LineChart state={chartData} />  */
+// {test_array.map(station => <button key={station}> {station} </button>)}
+    const onButtonClick = (sensor) => {
+        console.log("label",sensor)
+        var pos = null;
+        for(var i=0; i<chartData.datasets.length; i++) {
+            if(chartData.datasets[i].label === sensor) {
+                pos = i;
+            }
+        }
+
+        setChartData((prev) => ({
+            ...prev, 
+            pos: pos
+        }));
+    };
+    const value = 99
+    const classes = useStyles();
+ 
+    return (
+        <div className="App">
+            {chartData.datasets.map(station => 
+            <Button 
+            //classes = {{ root: classes.root}}
+            variant="contained" 
+            color= "primary"
+            className={station.label}
+            key={station.label} 
+            onClick={() => onButtonClick(station.label)}>
+                    {station.label}: {data.lvl}
+            </Button>)}
+
+            <header className="App-header">
+                <LineChart state={chartData} /> 
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -5,11 +5,11 @@ Chart.register(...registerables);
 const chartConfig = {
     type: 'line',
     data: {
-        labels: [1,2,3,4,5,6],
+        labels: [],
         datasets: [
             {
-                label: "sensor1",
-                data: [1,2,3,4,5,6],
+                label: "",
+                data: [],
                 backgroundColor: [
                 'rgba(75, 192, 192, 0.2)'
                 ],
@@ -31,6 +31,25 @@ const chartConfig = {
     }
 };
 
+const colorConfig = [
+    {
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)'
+    },
+    {
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)'
+    },
+    {
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)'
+    },
+    {
+        backgroundColor: 'rgba(255, 206, 86, 0.2)',
+        borderColor: 'rgba(255, 206, 86, 1)'
+    }      
+]
+
 const LineChart = (props) => {
     const chartContainer = useRef(null);
     const [chartInstance, setChartInstance] = useState(null);
@@ -44,28 +63,87 @@ const LineChart = (props) => {
         // updateDataset(0, [1, 2, 3, 4, 5, 6])
     }, [chartContainer]);
 
-    // useEffect(() => {
-    //     console.log("state", props.state);
-    // }, [props.state]);
+    // const datasets_structure = (label, data) => {
+    //     return({
+    //         label: label,
+    //         data: data,
+    //         backgroundColor: [
+    //         'rgba(75, 192, 192, 0.2)'
+    //         ],
+    //         borderColor: [
+    //         'rgba(75, 192, 192, 1)'
+    //         ],
+    //         borderWidth: 2,
+    //     })
+    // }
 
-    const updateDataset = (datasetIndex, newData) => {
-        console.log("data: ", chartInstance.data.datasets[0].data)
-        chartInstance.data.datasets[datasetIndex].data[5] = chartInstance.data.datasets[datasetIndex].data[5] + 1;
-        chartInstance.update();
-    };
+    // const update_dataset = (data,newValue,label) => {
+    //     var data_aux = data.slice();
+    //     if(data.length < 10) {
+    //         data_aux.push(newValue);
+    //     }
+    //     else {
+    //         for (var i = 0; i < 9; i++) {
+    //             data_aux[i] = data_aux[i+1];
+    //         }
+    //         data_aux[9] = newValue;
+    //     }
+    //     return datasets_structure(label,data_aux)
+    // }
 
-    const onButtonClick = () => {
-        const data = [1,2,3,4,5,6];
-        updateDataset(0, data);
-      };
+    useEffect(() => {
+        // const update_data = (state) => {
+        //     if(chartInstance.data.datasets[0].label === "") {
+        //         console.log("first message comming")
+        //         chartInstance.data.datasets[0].label = state.sensor;
+        //         chartInstance.data.datasets[0].data = [state.lvl];
+        //         //chartInstance.data.labels = [state.date];
+        //     }else{
+        //         var pos = null;
+        //         for(var i=0; i<chartInstance.data.datasets.length; i++) {
+        //             console.log("bucle ", chartInstance.data.datasets[i].label)
+        //             if(chartInstance.data.datasets[i].label === state.sensor){
+        //                 console.log("Posicion sensor:",state.sensor, i)
+        //                 pos = i;
+        //             }
+        //         }
+
+        //         if(pos == null) {
+        //             chartInstance.data.datasets.push(datasets_structure(state.sensor, [state.lvl]))
+        //             console.log("pos == null: ");
+        //         }
+        //         else {
+        //             //console.log("actualizarrrr",chartInstance.data.datasets[pos])
+        //             chartInstance.data.datasets[pos] = update_dataset(chartInstance.data.datasets[pos].data.slice(), state.lvl,state.sensor)
+        //         }
+        //         console.log("chartInstance ", chartInstance.data);
+        //     }
+        //     // chartInstance.data.labels = state.date;
+        //     // chartInstance.options.scales.yAxes.beginAtZero = true;
+        //     chartInstance.update();
+        // }
+
+        //console.log("state", props.state);
+        
+        if(chartInstance != null) {
+            console.log("props.state.labels[props.state.pos]", props.state.labels[props.state.pos])
+            console.log("props.state.datasets[props.state.pos]",props.state.datasets[props.state.pos])
+            chartInstance.data.labels = props.state.labels[props.state.pos]
+            chartInstance.data.datasets[0].data = props.state.datasets[props.state.pos].data
+            chartInstance.data.datasets[0].label = props.state.datasets[props.state.pos].label
+            chartInstance.data.datasets[0].backgroundColor = colorConfig[props.state.pos].backgroundColor
+            chartInstance.data.datasets[0].borderColor = colorConfig[props.state.pos].borderColor
+            chartInstance.options.scales.yAxes.beginAtZero = true
+            chartInstance.update()
+        }
+    }, [props.state]);
+
 //<button onClick={}>Randomize!</button>
     return (
         <div>
             <canvas ref={chartContainer} width={700} height={400}/>
-            <button onClick={onButtonClick}>Randomize!</button>
         </div>
     );
 };
-
 
 export default LineChart;
